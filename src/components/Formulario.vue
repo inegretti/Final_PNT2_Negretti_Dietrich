8bef6408ea1f0678b7f455baf93c0fb21688014c
inegretti <88805043+inegretti@users.noreply.github.com>
2022-12-13 20:17:02 -0300

    entrega

---
+++ src/components/Formulario.vue
@@ -0,0 +1,101 @@
+<template >
+
+  <section class="src-components-formulario">
+    <div class="jumbotron">
+    <h1>Conversor a Dolar</h1>
+    <hr>
+    <hr>
+    <br>
+    <vue-form :state="formState">
+ 
+         <br>
+          <validate tag="label">
+            <span>Monto: $ </span>
+            <input v-model="formData.monto" name="monto" type="number" required :min="1"/>
+            <field-messages name="monto" show="$dirty">
+              <div slot="required">Complete el campo</div>
+            </field-messages>
+          </validate>
+          <br>
+         <br>
+
+          <br>
+          <validate tag="label">
+            <span>Precio Dolar: $ </span>
+            <input v-model="formData.precio" name="precio" type="number" required :min="1"/>
+            <field-messages name="precio" show="$dirty">
+              <div slot="required">Complete el campo</div>
+            </field-messages>
+          </validate>
+          <br>
+         <br>
+  </vue-form>
+  <hr>
+  <span>Monto en U$D {{calcularMonto()}}</span> 
+  
+    </div>
+  </section>
+
+</template>
+
+<script >
+
+  export default  {
+    name: 'src-components-formulario',
+    props: [],
+    mounted () {
+      
+    },
+    data () {
+      return {
+        formState:{},
+        formData:this.getDataInicial(),
+        Min:3,
+        Max:15,
+        montoDolar:0
+      }
+    },
+    methods: {
+      enviar(){
+        this.formData= this.getDataInicial()
+        this.formState._reset()
+      },
+      getDataInicial(){
+         return {
+          monto:0,
+          precio:0
+         }
+      },
+      calcularMonto(){
+        let resp=0;
+        this.montoDolar=resp
+        if(this.formData.monto!=0 && this.formData.precio!=0){
+            resp=this.formData.monto/this.formData.precio; 
+        }
+        return resp
+      }
+      
+
+    },
+    computed: {
+
+    
+ }
+}
+
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-formulario {
+
+  }
+  .jumbotron{
+    background-color: green;
+    color: black;
+  }
+  hr{
+    background-color: black;
+  }
+</style>
---
+++ src/components/Respuestas.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <section class="src-components-http-client">
+     <div class="jumbotron">
+    <h1>Respuestas</h1>
+    <br>
+     <hr>
+    <table class="table table-dark">
+      <tr>
+        <th>Nro de pregunta</th>
+        <th>respuesta</th>
+        
+      </tr>
+     <tr v-for="(post,index) in posts" :key="index">
+    <td class="my-3 mr-3">{{post.preg}}</td>
+    <td class="my-3 mr-3">{{post.res}}</td>
+    
+    </tr>
+    </table>
+
+    </div>
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-http-client',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+      
+        posts:[ {preg:1,res:"c"},{preg:2,res:"b"},{preg:3,res:"c"},{preg:4,res:"a"},{preg:5,res:"b"}],
+
+      }
+    },
+    methods: {
+    
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+    .jumbotron{
+    background-color: red;
+    color: black;
+  }
+  hr{
+    background-color: black;
+  }
+</style>
